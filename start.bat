37e784533c59f3defab05cc4007eef5e5d728fbc
BowtieDev <rohan.dunn.rd@gmail.com>
2026-03-05 17:48:47 +0200

    Update start.bat for v1.0.2

--- start.bat
+++ start.bat
@@ -1,8 +1,9 @@
 cd..
-move Batch-1.0.1\compile.bat src
-move Batch-1.0.1\del.bat src
-copy Batch-1.0.1\run.bat bin
-move Batch-1.0.1\run.bat src
-move Batch-1.0.1\final_compile.bat
+move Batch-1.0.2\compile.bat src
+move Batch-1.0.2\del.bat src
+copy Batch-1.0.2\run.bat bin
+move Batch-1.0.2\run.bat src
+move Batch-1.0.2\final_compile.bat
 
 pause
+
