94257ab19b307f6e9badd82e27bc9ec81e523a99
BowtieDev <rohan.dunn.rd@gmail.com>
2026-03-05 17:29:52 +0200

    V1.0 commit

---
+++ final_compile.bat
@@ -0,0 +1,6 @@
+g++ src\main.cpp -o bin\main.exe
+del src\main.exe
+move src\compile.bat Batch
+move src\del.bat Batch
+move src\run.bat Batch
+move final_compile.bat Batch
---
+++ start.bat
@@ -0,0 +1,7 @@
+cd..
+move Batch\compile.bat src
+move Batch\del.bat src
+copy Batch\run.bat bin
+move Batch\run.bat src
+move Batch\final_compile.bat
+pause
