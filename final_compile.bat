f306e63c4530fbe4fe8074a9c3bcf18072a2c915
BowtieDev <rohan.dunn.rd@gmail.com>
2026-03-19 21:02:10 +0200

    Update final_compile.bat

--- final_compile.bat
+++ final_compile.bat
@@ -1,6 +1,4 @@
 g++ src\main.cpp -o bin\main.exe
 del src\main.exe
-move src\compile.bat Batch
-move src\del.bat Batch
-move src\run.bat Batch
-move final_compile.bat Batch
+move src\compile.bat Batch-1.0.3
+move final_compile.bat Batch-1.0.3tch
